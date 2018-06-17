60a652e30774cd5a8fd2a809005aea735d325c67
Abdulai Seidu <Abdulai.Seidu@outlook.com>
2018-06-17 14:11:27 +0000

    server.js update

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
